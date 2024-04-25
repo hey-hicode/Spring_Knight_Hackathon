ba200ae6e124ae1c4fdb05827eea08d481159919
hey-Hicode <kingdavies2021@gmail.com>
2024-04-25 19:44:41 +0100

    feat:vercel erro fix

--- Store/reducers/index.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import postSlice from "../slices/postSlice";
-
-const rootReducer = combineReducers({
-    post:postSlice,
-
-})
-
-export default rootReducer
