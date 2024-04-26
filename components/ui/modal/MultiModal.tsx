0f85ea45043b69163880ab56194a568255895922
hey-Hicode <kingdavies2021@gmail.com>
2024-04-26 15:01:25 +0100

    feat:fixed the sign up and login flow for both the investors and startups

--- components/ui/modal/MultiModal.tsx
+++ components/ui/modal/MultiModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
 const MultiModal = ({ isOpen, onClose }: any) => {
   const [mode, setMode] = useState("login"); // Can be 'login' or 'signup'
@@ -7,21 +7,21 @@ const MultiModal = ({ isOpen, onClose }: any) => {
 
   const navigateToSignupInvestor = () => {
     onClose(); // Close modal
-    router.push('/Auth/Signup/Investors'); // Navigate to investor signup page
+    router.push("/Auth/Signup/Investors"); // Navigate to investor signup page
   };
 
   const navigateToSignupStartup = () => {
     onClose(); // Close modal
-    router.push('/Auth/Signup/Startup'); // Navigate to startup signup page
+    router.push("/Auth/Signup/Startup"); // Navigate to startup signup page
   };
   const navigateToLoginInvestor = () => {
     onClose(); // Close modal
-    router.push('/Auth/Signup/Investors'); // Navigate to investor signup page
+    router.push("/Auth/Login/Investors"); // Navigate to investor Login page
   };
 
   const navigateToLoginStartup = () => {
     onClose(); // Close modal
-    router.push('/Auth/Signup/Startup'); // Navigate to startup signup page
+    router.push("/Auth/Login/Startup"); // Navigate to startup signup page
   };
 
   const switchToSignup = () => setMode("signup");
@@ -31,25 +31,31 @@ const MultiModal = ({ isOpen, onClose }: any) => {
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
-      <div className="fixed inset-0 bg-black bg-opacity-50  backdrop-blur-sm" onClick={onClose}></div>
+      <div
+        className="fixed inset-0 bg-black bg-opacity-50  backdrop-blur-sm"
+        onClick={onClose}
+      ></div>
       <div className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-      <h2  className="text-lg pb-3 font-medium leading-6 text-gray-900 capitalize">{mode} {'As '}</h2>
+        <h2 className="text-lg pb-3 font-medium leading-6 text-gray-900 capitalize">
+          {mode} {"As "}
+        </h2>
         {mode === "login" ? (
           <>
             <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8 w-full">
               <div className="h-32 rounded-lg relative bg-gray-200">
-                <div 
-                onClick={navigateToLoginInvestor}
-                className="inline-flex absolute bottom-0 justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
-                   An Investor
+                <div
+                  onClick={navigateToLoginInvestor}
+                  className="inline-flex absolute bottom-0 justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                >
+                  An Investor
                 </div>
               </div>
               <div className="h-32 rounded-lg relative bg-gray-200">
-                
                 <div
-                onClick={navigateToLoginStartup}
-                className="inline-flex absolute bottom-0 justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
-                   A Startup
+                  onClick={navigateToLoginStartup}
+                  className="inline-flex absolute bottom-0 justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                >
+                  A Startup
                 </div>
               </div>
             </div>
@@ -57,26 +63,31 @@ const MultiModal = ({ isOpen, onClose }: any) => {
               className="pt-3 flex justify-between items-center "
               onClick={switchToSignup}
             >
-              <span className="text-sm text-gray-500">Don't have an account? </span>
-              <span className="cursor-pointer text-sm  text-blue-500 ">Sign up</span>
+              <span className="text-sm text-gray-500">
+                Don't have an account?{" "}
+              </span>
+              <span className="cursor-pointer text-sm  text-blue-500 ">
+                Sign up
+              </span>
             </div>
           </>
         ) : (
           <>
-   
-
             <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8 w-full">
               <div className="h-32 rounded-lg relative bg-gray-200">
-                <div onClick={navigateToSignupInvestor} className="inline-flex absolute bottom-0 justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
-                   An Investor
+                <div
+                  onClick={navigateToSignupInvestor}
+                  className="inline-flex absolute bottom-0 justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                >
+                  An Investor
                 </div>
               </div>
               <div className="h-32 rounded-lg relative bg-gray-200">
-                
-                <div 
-                onClick={navigateToSignupStartup}
-                className="inline-flex absolute bottom-0 justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
-                   A Startup
+                <div
+                  onClick={navigateToSignupStartup}
+                  className="inline-flex absolute bottom-0 justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                >
+                  A Startup
                 </div>
               </div>
             </div>
@@ -84,8 +95,12 @@ const MultiModal = ({ isOpen, onClose }: any) => {
               className="pt-3 flex justify-between items-center "
               onClick={switchToLogin}
             >
-              <span className="text-sm text-gray-500">Already have an account?  </span>
-              <span className="cursor-pointer text-sm  text-blue-500 ">Log in!</span>
+              <span className="text-sm text-gray-500">
+                Already have an account?{" "}
+              </span>
+              <span className="cursor-pointer text-sm  text-blue-500 ">
+                Log in!
+              </span>
             </div>
           </>
         )}
